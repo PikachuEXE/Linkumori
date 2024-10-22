3262a667f947cf76eb080d2f58ba90e6fddc3e46
subham8907 <cc7642207@gmail.com>
2024-10-22 18:48:24 +0530

    push some changes in background script

--- background.js
+++ background.js
@@ -207,7 +207,7 @@ chrome.runtime.onInstalled.addListener(async () => {
 /// main fuction start here//
 /// here you can see it use dnr to enable and disable it//
 async function updateRuleSet(enabled) {
-  const allRulesets = ['ruleset_1', 'ruleset_2', 'ruleset_3', 'ruleset_4', 'ruleset_5', 'ruleset_6', 'ruleset_7','ruleset_8'];
+  const allRulesets = ['ruleset_1', 'ruleset_2', 'ruleset_3', 'ruleset_4', 'ruleset_5', 'ruleset_6', 'ruleset_7','ruleset_8','ruleset_9'];
 
   // Update static rulesets
   await chrome.declarativeNetRequest.updateEnabledRulesets({
